74863d798336efdf90a3d38e5610c1dad3c8d4ce
Manuel Martinez <jmmartinez@comunidad.unam.mx>
2016-10-25 14:59:20 -0500

    Corrección de ortografía

--- EjemploActividad6/Program.cs
+++ EjemploActividad6/Program.cs
@@ -12,33 +12,35 @@ namespace EjemploActividad6
         {
             //http://www.flickr.com/services/feeds/photos_public.gne?tags=soccer&format=json&nojsoncallback=1
 
-            //Task.Run es necesario porque las aplicaciones de consola no admiten async. Ej: async void Main(string[] args) no es posible. en las aplicaciones 
-            // para Windows Phone o Escritorio, no es necesario usar Task.Run
+            //Task.Run es necesario porque las aplicaciones de consola no admiten async. Ej: async void Main(string[] args) no es posible. 
+            //En las aplicaciones 
+            //para Windows Phone o Escritorio, no es necesario usar Task.Run
             Task.Run(async () =>
             {
                 //Inicializamos el objeto
                 using (HttpClient client = new HttpClient())
                 {
 
-                    //Realizamos una llama asincrona. En iOS tendriamos que usar callbacks con NSURLSession, y en Android AsyncTask con HttpUrlConnection. En este caso, async hace esta tarea mas sencilla.
-                    //content tendra el resultado como cadena (String). Despues hay que "desaplanar" esta cadena, esto es, pasarlo a una estructura de datos que podamos manipular
+                    //Realizamos una llama asíncrona. En iOS tendríamos que usar callbacks con NSURLSession, y en Android AsyncTask con HttpUrlConnection. En este caso, async hace esta tarea más sencilla.
+                    //content tendrá el resultado como cadena (String). Después hay que "desaplanar" esta cadena, esto es, pasarlo a una estructura de datos que podamos manipular
+
                     String content = await client.GetStringAsync("http://www.flickr.com/services/feeds/photos_public.gne?tags=soccer&format=json&nojsoncallback=1");
 
-                    //La libreria Newtonsoft, realiza el "desaplanado" de los datos. Tenemos que pasarle como parametros, el tipo de datos que esperamos, en este caso Feed y la cadena 
+                    //La librería Newtonsoft, realiza el "des aplanado" de los datos. Tenemos que pasarle como parámetros, el tipo de datos que esperamos, en este caso Feed y la cadena 
                     //que queremos desaplanar
                     Feed feed = JsonConvert.DeserializeObject<Feed>(content);
-                    //En este punto feed debera contener los datos del json
-                    //Para entender las clases que se crearon en la carpeta Models, tienen que abrir la liga del json en un navegador como Firefox o Chrome; y analisar su estructura.
+                    //En este punto feed deberá contener los datos del json
+                    //Para entender las clases que se crearon en la carpeta Models, tienen que abrir la url en un navegador como Firefox o Chrome; y analisar su estructura.
                     
                     //Imprimimos el titulo del feed
                     Console.WriteLine("Titulo: {0}",feed.Title);
 
-                    //Las imagenes estan en Feed -> Items [] -> Media -> M
+                    //Las imágenes estan en Feed -> Items [] -> Media -> M
                     foreach (var f in feed.Items) {
                         Console.WriteLine("Imagen: {0}",f.Media.M);
                     }
                     
-                    //Se termina la aplicacion con pulsar una tecla
+                    //Se termina la aplicación con pulsar una tecla
                     Console.ReadLine();
                 }
 
